b0a8553f549ca4648ec64b9b096aaa11786db4ae
Colin Woodbury <colin@fosskers.ca>
2022-03-10 11:11:14 -0800

    Generalise to accept any styling lambda

--- org-table-colour.el
+++ org-table-colour.el
@@ -22,31 +22,48 @@
 (require 'org-table)
 
 ;;;###autoload
-(defun org-table-colour-table ()
-  "Colour the 'org-mode' table at 'point'."
-  (interactive)
+(defun org-table-colour-table (get-face)
+  "Colour the 'org-mode' table at 'point', given a GET-FACE function.
+
+GET-FACE must accept a single numerical argument (the value of
+the cell) and return either a plist representing a face or nil.
+When nil, no styling of that cell will occur. Further, no styling
+will occur if the cell value is not a number.
+
+See `org-table-colour--colour-by-correlation' for an example."
   (let* ((lisp (org-table-to-lisp))
          (rows (length lisp))
          (cols (length (car lisp))))
     (save-excursion
-      (mapc (lambda (x) (mapc (lambda (y) (org-table-colour--colour-cell x y))
+      (mapc (lambda (x) (mapc (lambda (y) (org-table-colour--colour-cell get-face x y))
                               (number-sequence 2 rows)))
             (number-sequence 2 cols)))))
 
-(defun org-table-colour--colour-cell (x y)
-  "Colour the cell at the given X and Y coordinates."
+;;;###autoload
+(defun org-table-colour-correlation-matrix ()
+  "Colour the 'org-mode' table at 'point' that represents a Correlation Matrix."
+  (interactive)
+  (org-table-colour-table #'org-table-colour--colour-by-correlation))
+
+(defun org-table-colour--colour-cell (get-face x y)
+  "Colour the cell via a GET-FACE function at the given X and Y coordinates."
   (org-table-goto-line y)
   (org-table-goto-column x)
   (when-let* ((cell (org-table-get y x))
               (nmbr (string-to-number cell))
-              (face (cond ((>= nmbr 0.5) '(:foreground "black" :background "green"))
-                          ((>= nmbr 0.3) '(:foreground "black" :background "#90EE90"))
-                          ((<= nmbr -0.5) '(:foreground "black" :background "red"))
-                          ((<= nmbr -0.3) '(:foreground "black" :background "orange")))))
+              (face (funcall get-face nmbr)))
     (when face
       (let ((overlay (make-overlay (point)
                                    (progn (org-table-end-of-field 1) (point)))))
         (overlay-put overlay 'face face)))))
+
+(defun org-table-colour--colour-by-correlation (num)
+  "Colour a table cell NUM value assuming it's from a correlation matrix.
+Yields a plist that represents a face."
+  (cond ((>= num 0.5) '(:foreground "black" :background "green"))
+        ((>= num 0.3) '(:foreground "black" :background "#90EE90"))
+        ((<= num -0.5) '(:foreground "black" :background "red"))
+        ((<= num -0.3) '(:foreground "black" :background "orange"))))
 
 (provide 'org-table-colour)
 ;;; org-table-colour.el ends here
